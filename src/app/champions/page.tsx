b132bfcce63b59096a7ddffe9ec26654a36cbe0d
hj <hyeonjubaek06@gmail.com>
2025-03-18 03:18:59 +0900

    add: 챔피언 페이지에 정적 프로퍼티 추가

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://ddragon.leagueoflegends.com/cdn/15.5.1/data/ko_KR/champion.json"
+  );
+  const data = await res.json;
+};
 
 const ChampionsPage = () => {
-  return (
-    <div>챔피언 목록</div>
-  )
-}
+  return <div>챔피언 목록</div>;
+};
 
-export default ChampionsPage
+export default ChampionsPage;
