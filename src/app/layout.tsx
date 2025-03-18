0a1501615b74504d643faa0c83540c4a14ff497e
hj <hyeonjubaek06@gmail.com>
2025-03-19 01:26:03 +0900

    fix: layout.tsx 파일의 코드 스타일 개선

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
+// src/app/layout.tsx
+
 import Link from "next/link";
 import "./globals.css";
 import Providers from "./providers";
-
-
 
 export default function RootLayout({
   children,
@@ -13,13 +13,13 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>
-        <nav>
-          <Link href={"/"}>홈</Link>
-          <Link href={"/champions"}>챔피언 목록</Link>
-          <Link href={"/rotation"}>챔피언 로테이션</Link>
-          <Link href={"/items"}>아이템 목록</Link>
-        </nav>
-        {children}
+          <nav>
+            <Link href={"/"}>홈</Link>
+            <Link href={"/champions"}>챔피언 목록</Link>
+            <Link href={"/rotation"}>챔피언 로테이션</Link>
+            <Link href={"/items"}>아이템 목록</Link>
+          </nav>
+          {children}
         </Providers>
       </body>
     </html>
