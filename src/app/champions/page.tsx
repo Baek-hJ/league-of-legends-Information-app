8a2e85a5f2d36002bc5a4cd9454db19a8127f720
hj <hyeonjubaek06@gmail.com>
2025-03-19 01:26:24 +0900

    fix: champions 페이지의 코드 스타일 개선

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,26 +1,25 @@
 // src/app/champions/page.tsx
-'use client';
+"use client";
 
 import { ChampionCard } from "@/components/ChampionCard";
 // import { Champion } from "@/types/Champion";
 import { useDataQuery } from "../../../query/useDataQuery";
 
-
 const ChampionsPage = () => {
-  const {data: champions} = useDataQuery();
+  const { data: champions } = useDataQuery();
 
-  if (!champions) return <div>로딩 중...</div>
+  if (!champions) return <div>로딩 중...</div>;
 
   return (
-  <div>
-    <h1>챔피언 목록</h1>
     <div>
-      {champions.map((champion) => (
-        <ChampionCard key={champion.id} {...champion} />
-  ))}
+      <h1>챔피언 목록</h1>
+      <div>
+        {champions.map((champion) => (
+          <ChampionCard key={champion.id} {...champion} />
+        ))}
+      </div>
     </div>
-    </div>);
+  );
 };
 
 export default ChampionsPage;
-
