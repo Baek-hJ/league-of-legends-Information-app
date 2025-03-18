545f6525de0cba018d685e35111f06effd0c6975
hj <hyeonjubaek06@gmail.com>
2025-03-19 06:26:35 +0900

    style: ItemsPage 컴포넌트의 코드 정리 및 스타일 일관성 개선

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,27 +1,24 @@
 // src/app/items/page.tsx
 "use client";
 
-import { ItemCard } from '@/components/ItemCard';
-import { useItemDataQuery } from '../../../query/useItemDataQuery';
+import { ItemCard } from "@/components/ItemCard";
+import { useItemDataQuery } from "../../../query/useItemDataQuery";
 
 const ItemsPage = () => {
-
   const { data: ItemsPage } = useItemDataQuery();
 
   if (!ItemsPage) return <div>로딩 중...</div>;
 
   return (
     <div>
-      <h1 className='header-container'>
-      아이템 목록
-      </h1>
-      <div>
+      <h1 className="header-container">아이템 목록</h1>
+      <div className="item-card-container">
         {ItemsPage.map((item) => (
           <ItemCard key={item.name} {...item} />
         ))}
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
 export default ItemsPage;
