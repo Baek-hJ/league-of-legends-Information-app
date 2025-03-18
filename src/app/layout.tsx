99d6a8e1acf33ef4b4d663b3a748d8d54290ca1f
hj <hyeonjubaek06@gmail.com>
2025-03-19 02:43:53 +0900

    fix: 네비게이션에 클래스 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>
-          <nav>
+          <nav className="nav">
             <Link href={"/"}>홈</Link>
             <Link href={"/champions"}>챔피언 목록</Link>
             <Link href={"/rotation"}>챔피언 로테이션</Link>
