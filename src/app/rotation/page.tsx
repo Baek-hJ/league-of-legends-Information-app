5b8aadfd26230b01533517911d805f5bc494fd35
hj <hyeonjubaek06@gmail.com>
2025-03-19 07:12:03 +0900

    style: RotationPage 컴포넌트의 텍스트 스타일 개선

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -4,7 +4,7 @@ const RotationPage = () => {
   return (
     <div>
       <h1 className='header-container'>챔피언 로테이션
-      <p>(이번 주 무료로 플레이할 수 있어요!)</p>
+      <p className="text-xl">(이번 주 무료로 플레이할 수 있어요!)</p>
       </h1>
 
     </div>
