9f26e44a649842bd2e306fa02cd5e88dbb5d1d8a
hj <hyeonjubaek06@gmail.com>
2025-03-19 05:24:04 +0900

    feat: ItemCard 컴포넌트를 새 파일로 추가하여 아이템 정보를 표시하는 기능 구현

---
+++ src/components/ItemCard.tsx
@@ -0,0 +1,19 @@
+// src/components/ItemCard.tsx
+import { Item } from "@/types/Item";
+import Image from "next/image";
+
+export const ItemCard = ({ image, name, plaintext }: Item) => {
+  return (
+    <div>
+      <Image
+        src={image.full}
+        alt={name}
+        width={100}
+        height={100}
+        className="rounded-md mx-auto"
+      />
+      <h2 className="text-center mt-2 font-semibold">{name}</h2>
+      <p className="text-center text-sm text-gray-600">{plaintext}</p>
+    </div>
+  );
+};
