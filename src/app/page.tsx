d73d7354ee0f57982057ec7e2959897240198bdb
hj <hyeonjubaek06@gmail.com>
2025-03-19 03:10:57 +0900

    feat: 홈 페이지에 제목과 설명 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import './globals.css';
 
 export default function Home() {
   return (
-    <div className="home-container">Home</div>
+    <div className="home-container">
+      <h1 className='home-h1'>리그 오브 레전드 정보 앱</h1>
+      <p className='home-p'>Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.</p>
+    </div>
   );
 }
