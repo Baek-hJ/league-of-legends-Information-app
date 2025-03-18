4f2e8af42a72efed0992cd80e6437c1dccfd350e
hj <hyeonjubaek06@gmail.com>
2025-03-19 04:24:11 +0900

    fix: 챔피언 카드 컴포넌트에 ID 속성 추가 및 링크 수정

--- src/components/ChampionCard.tsx
+++ src/components/ChampionCard.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface CardProps {
+    id: string;
     image: {
         full: string;
     };
@@ -9,10 +10,10 @@ interface CardProps {
     title: string;
 };
 
-export const ChampionCard = ({image, name, title}: CardProps) => {
+export const ChampionCard = ({id, image, name, title}: CardProps) => {
     return (
         <div>
-            <Link href={`/champions/${name}`} className="block p-4 border rounded-lg hover:shadow-lg transition">
+            <Link href={`/champions/${id}`} className="block p-4 border rounded-lg hover:shadow-lg transition">
             <Image src={image.full} alt={name} width={200} height={200} className="rounded-md mx-auto"/>
             <h2 className="text-center mt-2 font-semibold">{name}</h2>
             <p className="text-center text-sm text-gray-600">{title}</p>
