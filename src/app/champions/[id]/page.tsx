04ebe42d00b0a4cd35c43dfce5cba27ab381969b
hj <hyeonjubaek06@gmail.com>
2025-03-19 10:29:24 +0900

    refactor: 불필요한 코드 제거 및 메타 태그 삭제

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -2,30 +2,15 @@
 import { fetchChampionDetail } from "@/utils/serverApi";
 import { ChampionDetail } from "@/types/Champion";
 import Image from "next/image";
-import Head from "next/head";
 
-export default async function ChampionDetailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function ChampionDetailPage({ params }: { params: { id: string } }) {
   const champion: ChampionDetail | null = await fetchChampionDetail(params.id);
+
   if (!champion)
-    return (
-      <p className="text-center text-red-500">챔피언을 찾을 수 없습니다.</p>
-    );
+    return <p className="text-center text-red-500">챔피언을 찾을 수 없습니다.</p>;
 
   return (
     <div className="container mx-auto p-8 text-center">
-
-      <Head>
-        <title>{champion.id} - 상세 정보</title>
-        <meta name="description" content={champion.blurb} />
-        <meta name="og:title" content={champion.name} />
-        <meta name="og:description" content={champion.blurb} />
-        <meta name="og:image" content={champion.image.full} />
-      </Head>
-
       <h1 className="text-3xl font-bold">{champion.name}</h1>
       <p className="text-lg text-gray-600">{champion.title}</p>
       <Image
