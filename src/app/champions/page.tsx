c72610ae6a8457e087da6280eb7241004994202a
hj <hyeonjubaek06@gmail.com>
2025-03-19 10:05:30 +0900

    feat: ISR 재생성 주기를 하루에서 제거하여 캐싱 전략 수정

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,8 +1,6 @@
 import { fetchChampionList } from "@/utils/serverApi";
 import { ChampionCard } from "@/components/ChampionCard";
 
-export const revalidate = 86400; // ISR 적용 (하루마다 재생성)
-
 export default async function ChampionsPage() {
   const champions = await fetchChampionList();
 
